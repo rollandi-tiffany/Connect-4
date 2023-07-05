f6663377c48a72351c8a45775c1b55ba16099dab
Tiffany Torres <torresrtiffany@gmail.com>
2023-07-04 21:47:05 -0700

    added variables, .each and .click function

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
 $(document).ready(function(){
-    const $circle = $(".circle");
+ 
+    let player = 1;
+    let winner = 0;
+    let count = 0;
+    let colors = {};
+    colors[1] = "red";
+    colors[2] = "yellow";
+
+    $(".circle").each(function(){
+        $(this).attr("id", count);
+        $(this).attr("data-player", 0);
+        count++;
+
+        $(this).click(function(){
+            if(isValid($(this).attr("id"))){
+                $(this).css("background-color", colors[player]);
+                $(this).attr("data-player", player);
+             
+
+
+            }
+
+            //function isValid
+
+        })
+
+    });
    
-})
+});
