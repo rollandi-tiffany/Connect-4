f69b2557ba3f5491aca9690d915709e4d025e3de
Tiffany Torres <torresrtiffany@gmail.com>
2023-07-08 09:51:37 -0700

    updated api

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-$(document).ready(function(){
-
+$(document).ready(function () {
+    const displayResult = document.getElementById("display-result");
+    let gameFinished = false;
 
     let player = 1;
     let winner = 0;
@@ -7,163 +8,165 @@ $(document).ready(function(){
     let colors = {};
     colors[-1] = "red";
     colors[1] = "yellow";
-   
-    
 
-    $("#restart").click(function(){
+    $("#restart").click(function () {
         clearBoard();
     });
 
-    function clearBoard(){
-        $(".circle").each(function(){
+    function clearBoard() {
+        $(".circle").each(function () {
             $(this).attr("data-player", 0);
             $(this).css("background-color", "white");
 
             winner = 0;
-
-        })
+            gameFinished = false;
+            displayResult.innerText = "";
+        });
     }
-    
 
-    $(".circle").each(function(){
+    $(".circle").each(function () {
         $(this).attr("id", count);
         $(this).attr("data-player", 0);
         count++;
 
-        $(this).click(function(){
-            console.log($(this).attr("id"));
-            if(isCircleFilled($(this).attr("id"))){
-                $(this).css("background-color", colors[player]);
-                $(this).attr("data-player", player);
-                if(checkWinner(player)){
-                    //alert("Congratulations " + colors[player] + " you win!");
-                    const $button = $("button");
-
-                $button.click((e) => {
-                    $.ajax({
-                        url:"https://api.thecatapi.com/v1/images/search?limit=10"
-                    }).then(function (date){
-                        console.log(date);
-                        addImageToScreen(data[0].url);
-                        $button.off();
-                    });
-                });
-                let addImageToScreen = (image) => {
-                    let $li = $(`<img src="${image}"alt="some image"/>`);
-
-        
-
-                }
-                   
-                    winner = player;
-
-
-            
-
-                }
-                
-
-                player *= -1;
-             
-
-
+        $(this).click(function () {
+            if (gameFinished || !isCircleFilled($(this).attr("id"))) {
+                return;
             }
 
-
-        
-//checking to see if circle is filled by red or yellow disc
-            function isCircleFilled(x){
-                let id = parseInt (x);
-                if(winner !== 0){
-                    return false;
-                }
-                
-                if($("#" + id).attr("data-player") ==="0"){
-                    return true;
-                }
-                if(id >=42){
-                    return true;
-                }
-                if($("#" + (id + 7)).attr("data-player") === 0){
-                    return false;
-                }
-                return false;
-            }
-//checking if the players make a horizontal or vertical win
-        function checkWinner(p){
-            let link = 0;
-            for(let r = 0; r < 42; r += 7){
-                for(let c = 0; c < 7; c++){
-                    let circle = $("#" + (r + c));
-                    if(circle.attr("data-player") === p.toString()){
-                       link ++;
-                    }else{
-                        link = 0;
-                    }
-                    if(link >= 4){
-                        return true;
-                    }
-                }
-                link = 0;
-
-            }
-        
-            for(let r = 0; r < 7; r++){
-                for(let c = 0; c < 42; c += 7){
-                   let circle = $("#" + (r + c));
-                    if(circle.attr("data-player") === p.toString()){
-                       link++;
-                    }else{
-                        link = 0;
-                   }
-                    if(link >= 4){
-                       return true;
-                    }
-                }
-                link = 0;
-            }
- // checking if players make a diagonal win from top-left to bottom-right or top-right to bottom-left           
-            for (let r = 0; r <=21; r += 7){
-                for (let c = 0; c <= 3; c++){
-                    let circle1 = $("#" + (r +c));
-                    let circle2 = $("#" + (r + c + 8));
-                    let circle3 = $("#" + (r + c + 16));
-                    let circle4 = $("#" + (r + c + 24));
-
-                    if (
-                        circle1.attr("data-player") === p.toString()&&
-                        circle2.attr("data-player") === p.toString()&&
-                        circle3.attr("data-player") === p.toString()&&
-                        circle4.attr("data-player") === p.toString()
-                    ){
-                        return true;
-                    }
-                }
+            $(this).css("background-color", colors[player]);
+            $(this).attr("data-player", player);
+            if (checkWinner(player)) {
+                displayWinner(player);
+                gameFinished = true;
+                return;
             }
 
-            for(let r = 0; r <= 21; r += 7){
-                for (let c = 6; c >= 3; c--){
-                    let circle1 = $("#" + (r + c));
-                    let circle2 = $("#" + (r + c + 6));
-                    let circle3 = $("#" + (r + c + 12));
-                    let circle4 = $("#" + (r + c +18));
-
-                    if(
-                        circle1.attr("data-player") === p.toString()&&
-                        circle2.attr("data-player") === p.toString()&&
-                        circle3.attr("data-player") === p.toString()&&
-                        circle4.attr("data-player") === p.toString()
-                    )
-                        return true;
-
-                }
-            }
-
-        }  
-        return false;
-
-        })
-
+            player *= -1;
+        });
     });
-   
+
+    function isCircleFilled(x) {
+        let id = parseInt(x);
+        if (winner !== 0) {
+            return false;
+        }
+
+        if ($("#" + id).attr("data-player") === "0") {
+            return true;
+        }
+        if (id >= 42) {
+            return true;
+        }
+        if ($("#" + (id + 7)).attr("data-player") === "0") {
+            return false;
+        }
+        return false;
+    }
+//checking if players make a horizontal or vertical win
+    function checkWinner(p) {
+        let link = 0;
+        for (let r = 0; r < 42; r += 7) {
+            for (let c = 0; c < 7; c++) {
+                let circle = $("#" + (r + c));
+                if (circle.attr("data-player") === p.toString()) {
+                    link++;
+                } else {
+                    link = 0;
+                }
+                if (link >= 4) {
+                    return true;
+                }
+            }
+            link = 0;
+        }
+
+        for (let r = 0; r < 7; r++) {
+            for (let c = 0; c < 42; c += 7) {
+                let circle = $("#" + (r + c));
+                if (circle.attr("data-player") === p.toString()) {
+                    link++;
+                } else {
+                    link = 0;
+                }
+                if (link >= 4) {
+                    return true;
+                }
+            }
+            link = 0;
+        }
+
+        // Checking if players make a diagonal win from top-left to bottom-right or top-right to bottom-left
+        for (let r = 0; r <= 21; r += 7) {
+            for (let c = 0; c <= 3; c++) {
+                let circle1 = $("#" + (r + c));
+                let circle2 = $("#" + (r + c + 8));
+                let circle3 = $("#" + (r + c + 16));
+                let circle4 = $("#" + (r + c + 24));
+
+                if (
+                    circle1.attr("data-player") === p.toString() &&
+                    circle2.attr("data-player") === p.toString() &&
+                    circle3.attr("data-player") === p.toString() &&
+                    circle4.attr("data-player") === p.toString()
+                ) {
+                    return true;
+                }
+            }
+        }
+
+        for (let r = 0; r <= 21; r += 7) {
+            for (let c = 6; c >= 3; c--) {
+                let circle1 = $("#" + (r + c));
+                let circle2 = $("#" + (r + c + 6));
+                let circle3 = $("#" + (r + c + 12));
+                let circle4 = $("#" + (r + c + 18));
+
+                if (
+                    circle1.attr("data-player") === p.toString() &&
+                    circle2.attr("data-player") === p.toString() &&
+                    circle3.attr("data-player") === p.toString() &&
+                    circle4.attr("data-player") === p.toString()
+                ) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    function displayWinner(player) {
+        const color = colors[player];
+        const message = "Congratulations " + color + ", you win!";
+        displayResult.innerText = message;
+
+        $.ajax({
+            url: "https://api.thecatapi.com/v1/images/search?limit=10",
+        })
+            .then(function (data) {
+                console.log(data);
+                addImageToScreen(data[0].url);
+            })
+            .catch(function (error) {
+                console.log("Error:", error);
+            });
+    }
+
+    function addImageToScreen(imageUrl) {
+        const $img = $('<img src="' + imageUrl + '" alt="Cat Image">');
+        displayResult.appendChild($img[0]);
+    }
 });
+
+
+
+
+
+
+
+
+
+
+
+
